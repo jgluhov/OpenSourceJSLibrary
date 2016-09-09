644ade8ec490a3e72d8afffd46f2e154e070ce25
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-09 10:09:30 +0300

    feat(random): Add ability to get an rarray of starwars names
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 var uniqueRandomArray = require('unique-random-array'),
-    starWarsNames = require('./starwars-names.json');
+  starWarsNames = require('./starwars-names.json'),
+  getRandomItem = uniqueRandomArray(starWarsNames);
 
 module.exports = {
-    all: starWarsNames,
-    random: uniqueRandomArray(starWarsNames)
+  all: starWarsNames,
+  random: random
 };
+
+function random(number) {
+  var randomItems = [],
+    i;
+
+  if (number === undefined) {
+    return getRandomItem();
+  }
+
+  for (i = 0; i < number; i++) {
+    randomItems.push(getRandomItem())
+  }
+  return randomItems;
+}
